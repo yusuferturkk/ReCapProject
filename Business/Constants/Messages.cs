3aba22c5ec224337fdd77afa5297b2274fcc5f1b
Yusuf Ertürk <yusuf_erturk98@hotmail.com>
2022-03-02 19:22:55 +0300

    14. Gün Odev2

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +16,14 @@ namespace Business.Constants
         public static string CarListed = "Araba listelendi";
         public static string CarNameInvalid = "Araba eklenemedi";
         public static string MaintenanceTime = "Sistem bakımda";
-        internal static string RentalAdded = "Araç kiralandı";
-        internal static string RentalError = "Araç kiralanamadı";
-        internal static string BrandAdded = "Araç adı eklendi";
+        public static string RentalAdded = "Araç kiralandı";
+        public static string RentalError = "Araç kiralanamadı";
+        public static string BrandAdded = "Araç adı eklendi";
+        public static string UserNotFound = "Kullanıcı bulunamadı!";
+        public static string PasswordError = "Şifre hatalı!";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı!";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi.";
+        public static string AccessTokenCreated = "Access Token başarıyla oluşturuldu";
     }
 }
